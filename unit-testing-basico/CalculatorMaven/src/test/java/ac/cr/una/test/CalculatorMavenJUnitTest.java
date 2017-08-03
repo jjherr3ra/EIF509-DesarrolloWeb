81ed2f15cf3d049229621486b19b94a09dbd6410
Maikol Guzman <mikeguzman@outlook.com>
2017-08-03 11:05:04 -0600

    Mas ejemplos de Unit Testing con JUnit
    Signed-off-by: Maikol Guzman <mikeguzman@outlook.com>

--- unit-testing-basico/CalculatorMaven/src/test/java/ac/cr/una/test/CalculatorMavenJUnitTest.java
+++ unit-testing-basico/CalculatorMaven/src/test/java/ac/cr/una/test/CalculatorMavenJUnitTest.java
@@ -6,9 +6,12 @@
 package ac.cr.una.test;
 
 import ac.cr.una.calculatormaven.Calculator;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 
 /**
  *
@@ -23,6 +26,31 @@ public class CalculatorMavenJUnitTest {
     public void setUp() {
     }
 
+        // Run once, e.g. Database connection, connection pool
+    @BeforeClass
+    public static void runOnceBeforeClass() {
+        System.out.println("@BeforeClass - runOnceBeforeClass");
+    }
+
+    // Run once, e.g close connection, cleanup
+    @AfterClass
+    public static void runOnceAfterClass() {
+        System.out.println("@AfterClass - runOnceAfterClass");
+    }
+
+    // Should rename to @BeforeTestMethod
+    // e.g. Creating an similar object and share for all @Test
+    @Before
+    public void runBeforeTestMethod() {
+        System.out.println("@Before - runBeforeTestMethod");
+    }
+
+    // Should rename to @AfterTestMethod
+    @After
+    public void runAfterTestMethod() {
+        System.out.println("@After - runAfterTestMethod");
+    }
+    
     /**
      * Test the Calculator
      */
@@ -31,5 +59,15 @@ public class CalculatorMavenJUnitTest {
         Calculator calculator = new Calculator();
         double result = calculator.add(10, 50);
         assertEquals(60, result, 0);
+    }
+    
+    @Test
+    public void test_method_1() {
+        System.out.println("@Test - test_method_1");
+    }
+
+    @Test
+    public void test_method_2() {
+        System.out.println("@Test - test_method_2");
     }
 }
